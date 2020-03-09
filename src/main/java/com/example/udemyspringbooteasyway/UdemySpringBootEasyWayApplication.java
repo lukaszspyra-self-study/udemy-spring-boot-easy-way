038dc8aacb0471410e54249e81aa9489e437dd1c
Lukasz Spyra <lukasz.spyrka@gmail.com>
2020-03-09 07:32:39 +0100

    Add Appication/Controller/View/Index files

---
+++ src/main/java/com/example/udemyspringbooteasyway/UdemySpringBootEasyWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemyspringbooteasyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringBootEasyWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemySpringBootEasyWayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/udemyspringbooteasyway/UdemySpringBootEasyWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udemyspringbooteasyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringBootEasyWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
